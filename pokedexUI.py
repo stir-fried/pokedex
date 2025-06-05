d9146a4430e4a5245ff1b21cadc14c087afd68d3
JavierC <104749212+stir-fried@users.noreply.github.com>
2025-06-05 11:49:26 -0400

    ui

---
+++ pokedexUI.py
@@ -0,0 +1,26 @@
+from customtkinter import *
+#CTkButton(master=app, text="Hello"
+#       ,corner_radius=10, width=120, height=32, fg_color="blue", hover_color="red",border_color="", border_width=
+#       command=fuct_name).pack(pady=20)
+#CTkLabel(master=app, text="Hello", font=("Arial", 24), text_color="white").pack(pady=20)
+#.place(relx=0.5, rely=0.5, anchor=CENTER)
+#.configure
+
+
+
+
+
+
+
+
+app = CTk()
+app.geometry("600x400")
+set_appearance_mode("dark")
+
+
+
+
+
+
+
+app.mainloop()
